63c8bdf2c56ec073eaaf4214db2912e07a539861
goku-byte511 <56932145+goku-byte511@users.noreply.github.com>
2020-10-05 00:23:55 +0530

    Create coin_change.java

---
+++ Dynamic_Programming/coin_change.java
@@ -0,0 +1,28 @@
+/* Dynamic Programming Java implementation of Coin 
+Change problem */
+import java.util.Arrays; 
+
+class CoinChange 
+{ 
+	static long countWays(int S[], int m, int n) 
+	{ 
+		long[] table = new long[n+1]; 
+
+		// Initialize all table values as 0 
+		Arrays.fill(table, 0); //O(n) 
+		table[0] = 1; 
+		for (int i=0; i<m; i++) 
+			for (int j=S[i]; j<=n; j++) 
+				table[j] += table[j-S[i]]; 
+
+		return table[n]; 
+	}  
+	public static void main(String args[]) 
+	{ 
+		int arr[] = {1, 2, 3}; 
+		int m = arr.length; 
+		int n = 4; 
+		System.out.println(countWays(arr, m, n)); 
+	} 
+} 
+
